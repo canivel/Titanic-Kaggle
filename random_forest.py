524e0f64ffa0261d4cdc1ff146350b2c55c7950f
Danilo Canivel <d.canivel@gmail.com>
2015-02-12 18:48:37 -0500

    add new LR / RF / SVM files

--- random_forest.py
+++ random_forest.py
@@ -72,20 +72,20 @@ title_list = ['Mrs', 'Mr', 'Master', 'Miss', 'Major', 'Rev', 'Dr', 'Ms', 'Mlle',
 df_train['Title'] = df_train['Name'].map(lambda x: substrings_in_string(x, title_list))
 df_train['Title'] = df_train.apply(replace_titles, axis=1)
 
-df_train['Title'][df_train.Title == 'Jonkheer'] = 'Master'
-df_train['Title'][df_train.Title.isin(['Ms', 'Mlle'])] = 'Miss'
-df_train['Title'][df_train.Title == 'Mme'] = 'Mrs'
-df_train['Title'][df_train.Title.isin(['Capt', 'Don', 'Major', 'Col', 'Sir'])] = 'Sir'
-df_train['Title'][df_train.Title.isin(['Dona', 'Lady', 'the Countess'])] = 'Lady'
+df_train['Title'].loc[df_train.Title == 'Jonkheer'] = 'Master'
+df_train['Title'].loc[df_train.Title.isin(['Ms', 'Mlle'])] = 'Miss'
+df_train['Title'].loc[df_train.Title == 'Mme'] = 'Mrs'
+df_train['Title'].loc[df_train.Title.isin(['Capt', 'Don', 'Major', 'Col', 'Sir'])] = 'Sir'
+df_train['Title'].loc[df_train.Title.isin(['Dona', 'Lady', 'the Countess'])] = 'Lady'
 
 df_test['Title'] = df_test['Name'].map(lambda x: substrings_in_string(x, title_list))
 df_test['Title'] = df_test.apply(replace_titles, axis=1)
 
-df_test['Title'][df_test.Title == 'Jonkheer'] = 'Master'
-df_test['Title'][df_test.Title.isin(['Ms', 'Mlle'])] = 'Miss'
-df_test['Title'][df_test.Title == 'Mme'] = 'Mrs'
-df_test['Title'][df_test.Title.isin(['Capt', 'Don', 'Major', 'Col', 'Sir'])] = 'Sir'
-df_test['Title'][df_test.Title.isin(['Dona', 'Lady', 'the Countess'])] = 'Lady'
+df_test['Title'].loc[df_test.Title == 'Jonkheer'] = 'Master'
+df_test['Title'].loc[df_test.Title.isin(['Ms', 'Mlle'])] = 'Miss'
+df_test['Title'].loc[df_test.Title == 'Mme'] = 'Mrs'
+df_test['Title'].loc[df_test.Title.isin(['Capt', 'Don', 'Major', 'Col', 'Sir'])] = 'Sir'
+df_test['Title'].loc[df_test.Title.isin(['Dona', 'Lady', 'the Countess'])] = 'Lady'
 
 
 #Turning cabin number into Deck
@@ -143,14 +143,14 @@ age_df_test.loc[(age_df_test['Age'].isnull()), 'Age'] = predictedAges
 # df_test['Age'].dropna()
 df_train['AgeFullFill'] = age_df_train['Age']
 df_train['AgeName'] = ""
-df_train['AgeName'][df_train.AgeFullFill <= 5] = 1
-df_train['AgeName'][(df_train.AgeFullFill > 5) & (df_train.AgeFullFill < 15)] = 3
-df_train['AgeName'][(df_train.AgeFullFill >= 15) & (df_train.AgeFullFill < 60)] = 4
-df_train['AgeName'][df_train.AgeFullFill >= 60] = 2
+df_train['AgeName'].loc[df_train.AgeFullFill <= 5] = 1
+df_train['AgeName'].loc[(df_train.AgeFullFill > 5) & (df_train.AgeFullFill < 15)] = 3
+df_train['AgeName'].loc[(df_train.AgeFullFill >= 15) & (df_train.AgeFullFill < 60)] = 4
+df_train['AgeName'].loc[df_train.AgeFullFill >= 60] = 2
 
 df_train['HighLow'] = df_train['Pclass']
-df_train['HighLow'][(df_train.Fare_Per_Person < 8)] = 1
-df_train['HighLow'][(df_train.Fare_Per_Person >= 8)] = 2
+df_train['HighLow'].loc[(df_train.Fare_Per_Person < 8)] = 1
+df_train['HighLow'].loc[(df_train.Fare_Per_Person >= 8)] = 2
 
 
 df_train['Age*Class'] = df_train['AgeFullFill'] * df_train.Pclass
@@ -158,66 +158,21 @@ df_train['Age*Cabin'] = df_train['AgeFullFill'] * df_train['Deck_N']
 df_train['Title*FarePP'] = df_train['Title_N'] * df_train['Fare_Per_Person']
 
 
-
 df_test['AgeFullFill'] = age_df_test['Age']
 df_test['AgeName'] = ""
-df_test['AgeName'][df_test.AgeFullFill <= 5] = 1
-df_test['AgeName'][(df_test.AgeFullFill > 5) & (df_test.AgeFullFill < 15)] = 3
-df_test['AgeName'][(df_test.AgeFullFill >= 15) & (df_test.AgeFullFill < 60)] = 4
-df_test['AgeName'][df_test.AgeFullFill >= 60] = 2
+df_test['AgeName'].loc[df_test.AgeFullFill <= 5] = 1
+df_test['AgeName'].loc[(df_test.AgeFullFill > 5) & (df_test.AgeFullFill < 15)] = 3
+df_test['AgeName'].loc[(df_test.AgeFullFill >= 15) & (df_test.AgeFullFill < 60)] = 4
+df_test['AgeName'].loc[df_test.AgeFullFill >= 60] = 2
 
 df_test['HighLow'] = df_test['Pclass']
-df_test['HighLow'][(df_test.Fare_Per_Person < 8)] = 1
-df_test['HighLow'][(df_test.Fare_Per_Person >= 8)] = 2
+df_test['HighLow'].loc[(df_test.Fare_Per_Person < 8)] = 1
+df_test['HighLow'].loc[(df_test.Fare_Per_Person >= 8)] = 2
 
 df_test['Age*Class'] = df_test['AgeFullFill'] * df_test.Pclass
 df_test['Age*Cabin'] = df_test['AgeFullFill'] * df_test['Deck_N']
 df_test['Title*FarePP'] = df_test['Title_N'] * df_test['Fare_Per_Person']
 
-
-
-
-
-# median_ages = np.zeros((2, 3))
-#
-# for i in range(0, 2):
-#     for j in range(0, 3):
-#         median_ages[i, j] = df_train[(df_train['Gender'] == i) & \
-#                                (df_train['Pclass'] == j + 1)]['Age'].dropna().median()
-#
-# for i in range(0, 2):
-#     for j in range(0, 3):
-#         median_ages[i, j] = df_test[(df_test['Gender'] == i) & \
-#                                (df_test['Pclass'] == j + 1)]['Age'].dropna().median()
-
-# df_train['AgeFill'] = df_train['Age']
-# df_test['AgeFill'] = df_test['Age']
-
-# for i in range(0, 2):
-#     for j in range(0, 3):
-#         df_train.loc[(df_train.Age.isnull()) & (df_train.Gender == i) & (df_train.Pclass == j + 1), \
-#                'AgeFill'] = median_ages[i, j]
-#
-#
-# for i in range(0, 2):
-#     for j in range(0, 3):
-#         df_test.loc[(df_test.Age.isnull()) & (df_test.Gender == i) & (df_test.Pclass == j + 1), \
-#                'AgeFill'] = median_ages[i, j]
-
-# df_train['AgeIsNull'] = pd.isnull(df_train.Age).astype(int)
-# df_test['AgeIsNull'] = pd.isnull(df_test.Age).astype(int)
-
-
-# print df.dtypes[df.dtypes.map(lambda x: x=='object')]
-#
-# print list(df_train.columns.values)
-# exit()
-# df_train = df_train.drop(['Sex', 'Embarked', 'Fare', 'Ticket', 'Cabin', 'Name', 'Title', 'Deck'], axis=1)
-# df_train = df_train.dropna()
-# df_test = df_test.drop(['Sex', 'Embarked', 'Fare', 'Ticket', 'Cabin', 'Name', 'Title', 'Deck'], axis=1)
-# Find if there are any nans
-# print df_test[pd.isnull(df_test).any(axis=1)]
-# exit()
 
 FEATURES = ['Pclass', 'SibSp', 'Parch', 'Gender', 'Port_of_Embarkation', 'AgeFullFill',
             'FamilySize', 'Age*Class', 'Deck_N', 'Title_N', 'Fare_Per_Person', 'AgeName', 'HighLow']
@@ -242,64 +197,11 @@ data_test = df_test
 # features_train_scaled = min_max_scaler.fit_transform(features_train)
 # features_test_scaled = min_max_scaler.fit_transform(features_test)
 
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-features_train_scaled = scaler.fit_transform(features_train)
-features_test_scaled = scaler.fit_transform(features_test)
+# from sklearn.preprocessing import StandardScaler
+# scaler = StandardScaler()
+# features_train_scaled = scaler.fit_transform(features_train)
+# features_test_scaled = scaler.fit_transform(features_test)
 
-from sklearn.pipeline import Pipeline
-from sklearn.decomposition import RandomizedPCA
-
-
-# from sklearn import linear_model
-# logreg = linear_model.LogisticRegression(C=1000)
-# clf = logreg.fit(features_train, labels_train)
-# pred = clf.predict(features_test)
-# print pred
-# print accuracy_score(labels_test, pred)
-
-### 0.784688995215
-# print "Decision Tree"
-# param_grid = {'criterion': ('gini', 'entropy'),
-#               'splitter': ('best', 'random'),
-#               'min_samples_split': [4, 5, 10, 20],
-#               'max_features': ('auto', 'sqrt', 'log2', None),
-#               'max_depth': [None, 1, 2, 10, 50],
-#               'max_leaf_nodes': [None, 8]}
-# clf = grid_search.GridSearchCV(DecisionTreeClassifier(random_state=42), param_grid)
-#
-# clf = clf.fit(features_train, labels_train)
-# print(clf.best_estimator_)
-# pred = clf.predict(features_test)
-# print pred
-# print accuracy_score(labels_test, pred)
-# print "Random Forest"
-# param_grid = {'criterion': ('gini', 'entropy'),
-#               'min_samples_split': [1, 2, 10, 20],
-#               'max_features': (5, 8, 'auto', None),
-#               'max_depth': [None, 2, 10],
-#               'n_estimators': [1000, 2000],
-#               'max_leaf_nodes': [8, 16, 32]}
-'''
-0.782296650718
-clf = RandomForestClassifier(bootstrap=True,
-             criterion='entropy', max_depth=None, max_features=2,
-             max_leaf_nodes=8, min_samples_split=1, n_estimators=1000,
-             n_jobs=-1, oob_score=False)
-
-0.791866028708
-clf = RandomForestClassifier(bootstrap=True,
-            criterion='entropy', max_depth=None, max_features=2,
-            max_leaf_nodes=8, min_samples_split=1, n_estimators=1000,
-            n_jobs=-1, oob_score=False)
-'''
-# pipeline = RandomForestClassifier(n_estimators=100, random_state=42)
-# clf = grid_search.GridSearchCV(pipeline, param_grid=param_grid, verbose=3, scoring='accuracy', cv=10)
-# clf = clf.fit(features_train, labels_train)
-# pred = clf.predict(features_test)
-# print(clf.best_estimator_)
-# acc = accuracy_score(labels_test, pred)
-# print acc
 
 # print "Rough fitting a RandomForest to determine feature importance..."
 #
@@ -316,15 +218,9 @@ ids = df_test['PassengerId'].values
 def brute_force_acc_rd(features_train, labels_train, features_test, labels_test, ids):
 
     clf = RandomForestClassifier(bootstrap=True,
-            criterion='entropy', max_depth=None, max_features=5,
-            max_leaf_nodes=8, min_samples_split=1, n_estimators=1000,
+            criterion='gini', max_depth=None, max_features=2,
+            max_leaf_nodes=8, min_samples_split=10, n_estimators=1000,
             n_jobs=-1, oob_score=False)
-
-    # clf = RandomForestClassifier(bootstrap=True,
-    #         criterion='entropy', max_depth=None, max_features=2,
-    #         max_leaf_nodes=8, min_samples_split=1, n_estimators=1000,
-    #         n_jobs=-1, oob_score=False)
-
 
     clf = clf.fit(features_train, labels_train)
     # print(clf.best_estimator_)
@@ -335,8 +231,8 @@ def brute_force_acc_rd(features_train, labels_train, features_test, labels_test,
     #     print acc
 
     print acc
-    importances = clf.feature_importances_
-    print importances
+    # importances = clf.feature_importances_
+    # print importances
 
 
     if(acc > 0.85):
