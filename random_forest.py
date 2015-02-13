600e4bd26224feab656d73e81a0eab774a5d85af
Danilo Canivel <d.canivel@gmail.com>
2015-02-12 21:22:33 -0500

    +80%

--- random_forest.py
+++ random_forest.py
@@ -54,7 +54,7 @@ def substrings_in_string(big_string, substrings):
 
 def replace_titles(x):
     title=x['Title']
-    if title in ['Don', 'Major', 'Capt', 'Jonkheer', 'Rev', 'Col']:
+    if title in ['Don', 'Major', 'Capt', 'Jonkheer', 'Rev', 'Col', 'Master']:
         return 'Mr'
     elif title in ['Countess', 'Mme']:
         return 'Mrs'
@@ -70,23 +70,30 @@ def replace_titles(x):
 
 title_list = ['Mrs', 'Mr', 'Master', 'Miss', 'Major', 'Rev', 'Dr', 'Ms', 'Mlle', 'Col', 'Capt', 'Mme', 'Countess', 'Don', 'Jonkheer']
 df_train['Title'] = df_train['Name'].map(lambda x: substrings_in_string(x, title_list))
-df_train['Title'] = df_train.apply(replace_titles, axis=1)
+# df_train['Title'] = df_train.apply(replace_titles, axis=1)
 
 df_train['Title'].loc[df_train.Title == 'Jonkheer'] = 'Master'
 df_train['Title'].loc[df_train.Title.isin(['Ms', 'Mlle'])] = 'Miss'
 df_train['Title'].loc[df_train.Title == 'Mme'] = 'Mrs'
-df_train['Title'].loc[df_train.Title.isin(['Capt', 'Don', 'Major', 'Col', 'Sir'])] = 'Sir'
-df_train['Title'].loc[df_train.Title.isin(['Dona', 'Lady', 'the Countess'])] = 'Lady'
+df_train['Title'].loc[df_train.Title.isin(['Capt', 'Don', 'Major', 'Col', 'Sir', 'Rev', 'Dr'])] = 'Sir'
+df_train['Title'].loc[df_train.Title.isin(['Dona', 'Countess'])] = 'Lady'
+
+df_train['Title_N'] = df_train['Title'].map({'Mrs': 1, 'Mr': 2, 'Sir': 3, 'Miss': 4, 'Lady': 5, 'Master':6})
 
 df_test['Title'] = df_test['Name'].map(lambda x: substrings_in_string(x, title_list))
-df_test['Title'] = df_test.apply(replace_titles, axis=1)
+# df_test['Title'] = df_test.apply(replace_titles, axis=1)
+
+# df_train.to_csv("data_train2.tst", "\t")
+# df_test.to_csv("data_test2.tst", "\t")
+# exit()
 
 df_test['Title'].loc[df_test.Title == 'Jonkheer'] = 'Master'
 df_test['Title'].loc[df_test.Title.isin(['Ms', 'Mlle'])] = 'Miss'
 df_test['Title'].loc[df_test.Title == 'Mme'] = 'Mrs'
-df_test['Title'].loc[df_test.Title.isin(['Capt', 'Don', 'Major', 'Col', 'Sir'])] = 'Sir'
-df_test['Title'].loc[df_test.Title.isin(['Dona', 'Lady', 'the Countess'])] = 'Lady'
+df_test['Title'].loc[df_test.Title.isin(['Capt', 'Don', 'Major', 'Col', 'Sir', 'Rev', 'Dr'])] = 'Sir'
+df_test['Title'].loc[df_test.Title.isin(['Dona', 'Countess'])] = 'Lady'
 
+df_test['Title_N'] = df_test['Title'].map({'Mrs': 1, 'Mr': 2, 'Sir': 3, 'Miss': 4, 'Lady': 5, 'Master':6})
 
 #Turning cabin number into Deck
 df_train['Cabin'].fillna('Unknown', inplace=True)
@@ -101,12 +108,9 @@ df_train['Port_of_Embarkation'] = df_train['Embarked'].map({'C': 1, 'Q': 2, 'S':
 df_train['Deck_N'] = df_train['Deck'].map({'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'T': 7, 'G': 8, 'Unknown': 9})
 df_train['Title_N'] = df_train['Title'].map({'Mrs': 1, 'Mr': 2, 'Master': 3, 'Miss': 4, 'Sir': 5, 'Rev': 6,
                                              'Dr': 7, 'Lady': 8})
-
 df_test['Gender'] = df_test['Sex'].map({'female': 0, 'male': 1}).astype(int)
 df_test['Port_of_Embarkation'] = df_test['Embarked'].map({'C': 1, 'Q': 2, 'S': 3})
 df_test['Deck_N'] = df_test['Deck'].map({'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'T': 7, 'G': 8, 'Unknown': 9})
-df_test['Title_N'] = df_test['Title'].map({'Mrs': 1, 'Mr': 2, 'Master': 3, 'Miss': 4, 'Sir': 5, 'Rev': 6,
-                                           'Dr': 7, 'Lady': 8})
 
 df_train['FamilySize'] = df_train['SibSp'] + df_train['Parch']
 df_train['Fare_Per_Person']=df_train['Fare']/(df_train['FamilySize']+1)
@@ -151,10 +155,19 @@ df_train['HighLow'] = df_train['Pclass']
 df_train['HighLow'].loc[(df_train.Fare_Per_Person < 52)] = 1 # people with fare 52 or less die more
 df_train['HighLow'].loc[(df_train.Fare_Per_Person >= 52)] = 2
 
+df_train['HighLow*Gender'] = df_train['Pclass']
+df_train['HighLow*Gender'].loc[(df_train.Fare_Per_Person < 52) & (df_train.Gender == 0)] = 1 # man poor
+df_train['HighLow*Gender'].loc[(df_train.Fare_Per_Person >= 52) & (df_train.Gender == 0)] = 2 #man rich
+df_train['HighLow*Gender'].loc[(df_train.Fare_Per_Person < 52) & (df_train.Gender == 1)] = 3 # woman poor
+df_train['HighLow*Gender'].loc[(df_train.Fare_Per_Person >= 52) & (df_train.Gender == 1)] = 4 #woman rich
+
 df_train['AgeName*HighLow'] = ""
 df_train['AgeName*HighLow'] = df_train['AgeName']/df_train['HighLow']
 df_train['AgeName*HighLow'] = df_train['AgeName']/df_train['HighLow']
 
+df_train['AgeFullFill*HighLow'] = ""
+df_train['AgeFullFill*HighLow'] = df_train['HighLow']/df_train['AgeFullFill']
+df_train['AgeFullFill*HighLow'] = df_train['HighLow']/df_train['AgeFullFill']
 
 df_train['Age*Class'] = df_train['AgeFullFill'] * df_train.Pclass
 df_train['Age*Cabin'] = df_train['AgeFullFill'] * df_train['Deck_N']
@@ -171,9 +184,19 @@ df_test['HighLow'] = df_test['Pclass']
 df_test['HighLow'].loc[(df_test.Fare_Per_Person < 52)] = 1 # people with fare 52 or less die more
 df_test['HighLow'].loc[(df_test.Fare_Per_Person >= 52)] = 2
 
+df_test['HighLow*Gender'] = df_test['Pclass']
+df_test['HighLow*Gender'].loc[(df_test.Fare_Per_Person < 52) & (df_test.Gender == 0)] = 1 # man poor
+df_test['HighLow*Gender'].loc[(df_test.Fare_Per_Person >= 52) & (df_test.Gender == 0)] = 2 #man rich
+df_test['HighLow*Gender'].loc[(df_test.Fare_Per_Person < 52) & (df_test.Gender == 1)] = 3 # woman poor
+df_test['HighLow*Gender'].loc[(df_test.Fare_Per_Person >= 52) & (df_test.Gender == 1)] = 4 #woman rich
+
 df_test['AgeName*HighLow'] = ""
 df_test['AgeName*HighLow'] = df_test['AgeName']/df_test['HighLow']
 df_test['AgeName*HighLow'] = df_test['AgeName']/df_test['HighLow']
+
+df_test['AgeFullFill*HighLow'] = ""
+df_test['AgeFullFill*HighLow'] = df_test['HighLow']/df_test['AgeFullFill']
+df_test['AgeFullFill*HighLow'] = df_test['HighLow']/df_test['AgeFullFill']
 
 df_test['Age*Class'] = df_test['AgeFullFill'] * df_test.Pclass
 df_test['Age*Cabin'] = df_test['AgeFullFill'] * df_test['Deck_N']
@@ -183,8 +206,8 @@ df_test['Title*FarePP'] = df_test['Title_N'] * df_test['Fare_Per_Person']
 # FEATURES = ['Pclass', 'SibSp', 'Parch', 'Gender', 'Port_of_Embarkation', 'AgeFullFill',
 #             'FamilySize', 'Age*Class', 'Deck_N', 'Title_N', 'Fare_Per_Person', 'AgeName', 'HighLow']
 
-FEATURES = ['Pclass', 'SibSp', 'Gender', 'AgeFullFill',
-            'FamilySize', 'Age*Class', 'Deck_N', 'Title_N', 'Fare_Per_Person', 'HighLow', 'AgeName*HighLow']
+FEATURES = ['Pclass', 'Gender', 'AgeFullFill', 'Port_of_Embarkation',
+            'FamilySize', 'Age*Class', 'Deck_N', 'Title_N', 'Fare_Per_Person', 'AgeFullFill*HighLow']
 
 features_train = np.array(df_train[FEATURES].values)
 labels_train = df_train["Survived"]
@@ -218,13 +241,20 @@ data_test = df_test
 # print feature_importance
 # exit()
 
+#best til now 851pm 2/12/15
+# clf = RandomForestClassifier(bootstrap=True,
+#             criterion='entropy', max_depth=None, max_features=2,
+#             max_leaf_nodes=16, min_samples_split=10, n_estimators=1000,
+#             n_jobs=-1, oob_score=False)
+
+
 
 ids = df_test['PassengerId'].values
 
 def brute_force_acc_rd(features_train, labels_train, features_test, labels_test, ids):
 
     clf = RandomForestClassifier(bootstrap=True,
-            criterion='entropy', max_depth=None, max_features=2,
+            criterion='entropy', max_depth=20, max_features=2,
             max_leaf_nodes=16, min_samples_split=10, n_estimators=1000,
             n_jobs=-1, oob_score=False)
 
@@ -238,17 +268,18 @@ def brute_force_acc_rd(features_train, labels_train, features_test, labels_test,
 
     print acc
     feature_importance = clf.feature_importances_
-    if(acc > 0.79):
+    if(acc > 0.80):
         feature_importance = 100.0 * (feature_importance / feature_importance.max())
         print feature_importance
 
 
-    if(acc > 0.85):
-        predictions_file = open("data/canivel_random_forest_81.csv", "wb")
+    if(acc > 0.82):
+        predictions_file = open("data/canivel_random_forest_82.csv", "wb")
         predictions_file_object = csv.writer(predictions_file)
         predictions_file_object.writerow(["PassengerId", "Survived"])
         predictions_file_object.writerows(zip(ids, pred))
         predictions_file.close()
+        print ("NEW FILE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! YEA!!!!")
 
     return acc
 
